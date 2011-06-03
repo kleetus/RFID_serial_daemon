4b88f79f22945f88a71483a01ddf45c9fb9b462c
kleetus <kleetus@debian.corp.primedia.com>
2011-06-03 11:42:19 -0400

    fixed issues with daemon

--- daemon.c
+++ daemon.c
@@ -14,8 +14,8 @@
 #include <pty.h>
 #include <time.h>
 
-#define TABLESIZE 1000000
-#define DBLINESIZE 14 //this should be the real size of the db string without a newline or null termination character
+#define TABLESIZE 10000000
+#define DBLINESIZE 13 //this should be the real size of the db string without a newline or null termination character
 #define BAUDRATE B9600
 #define _POSIX_SOURCE 1
 #define FALSE 0
@@ -84,37 +84,37 @@ signal_handler_IO(int status) {
 	h=hash(str);
   
   if(h >= TABLESIZE) {
-		ans = '4';
-		goto error_condition;
+    ans = '4';
+    goto error_condition;
   }
     
-	struct simple_rfid_access rf = db[h];
+  struct simple_rfid_access rf = db[h];
 
-	while(1) {
-		if(strcmp(rf.cardnum,str) == 0) { ans = db[h].hashval; break; }
-		else if(rf.next != NULL) {
-			rf = *(rf.next);
-		}
-		else {
-			ans = '5';
-			break;
-		}
-	}
+  while(1) {
+    if(strcmp(rf.cardnum,str) == 0) { ans = db[h].hashval; break; }
+    else if(rf.next != NULL) {
+      rf = *(rf.next);
+    }
+    else {
+      ans = '5';
+      break;
+    }
+  }
 error_condition:
   if(strcmp(str, "3400C2DF0B22") == 0){ans='1';}//this is the admin key in case the database is destroyed or unavailable
-	sprintf(logbuf, "IO HANDLER -- received: %s +++ answered with: %c", str, ans);
-	logdaemonevent(logbuf);
-	sprintf(ansstr, "%c@%c", ans, station_id);
-	write(fd, ansstr, 3);
-}
+    sprintf(logbuf, "IO HANDLER -- received: %s +++ answered with: %c", str, ans);
+    logdaemonevent(logbuf);
+    sprintf(ansstr, "%c@%c", ans, station_id);
+    write(fd, ansstr, 3);
+  }
 
 void
 clear() {
   struct simple_rfid_access rfiddb;
   int i;
-	rfiddb.cardnum = "";
-	rfiddb.hashval = 0;
-	rfiddb.next = NULL;
+  rfiddb.cardnum = "";
+  rfiddb.hashval = 0;
+  rfiddb.next = NULL;
   for(i=0; i<TABLESIZE; i++) {
    db[i] = rfiddb;
   }
@@ -123,87 +123,80 @@ clear() {
 int
 load(int argc, char **argv) {
   
-  
   #ifdef TESTING
   /* for testing only */
   int i,h, collisioncnt;
   for(i=1; i<5000; i++) {	
-		char *n = calloc(5, sizeof(char));
-
-		sprintf(n, "%i", i);
-		
-		h = hash(n);
-		
-		if(strcmp(n, db[h].cardnum) == 0) {
-			//collision!
-			struct simple_rfid_access *rfiddb = calloc(1, sizeof(struct simple_rfid_access));			
+    char *n = calloc(5, sizeof(char));
+    sprintf(n, "%i", i);
+    h = hash(n);
+    if(strcmp(n, db[h].cardnum) == 0) {
+      //collision!
+      struct simple_rfid_access *rfiddb = calloc(1, sizeof(struct simple_rfid_access));			
       db[h].next = rfiddb;
-			rfiddb->cardnum = n;
-			rfiddb->collisioncnt++;
-			rfiddb->hashval = '1'; //just hardcoded to 'yes'
+      rfiddb->cardnum = n;
+      rfiddb->collisioncnt++;
+      rfiddb->hashval = '1'; //just hardcoded to 'yes'
     }
     else {
       db[h].cardnum = n;
-			db[h].hashval = '1';
-			db[h].collisioncnt = 0;
+      db[h].hashval = '1';
+      db[h].collisioncnt = 0;
     }
-	}
+  }
   /* end test */
   
   
   #else
   FILE *fp;
-	char *str;
+  char *str;
   char buf[DBLINESIZE+1];
-	int h, collisioncnt;
-	char ans;
-	char logentry[256];
+  int h, collisioncnt;
+  char ans;
+  char logentry[256];
 	
   if(argc>2) {
-		fp = fopen(argv[2], "r");
-		device = argv[1];
-	}
+    fp = fopen(argv[2], "r");
+    device = argv[1];
+  }
   else {
-		fp = fopen("/root/RFID_serial_daemon/db_real.txt", "r");
-		device = "/dev/ttyS0";
-	}
+    fp = fopen("./db_real.txt", "r");
+    device = "/dev/ttyS0";
+  }
   
   if(fp == NULL) {
     printf("failed to open database.\n");
-		exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 
   while(fgets(buf, sizeof(buf), fp)) {
-		if(*buf == '\n') continue;
-		
-		//this memory never gets freed and shouldn't
-		str = calloc(DBLINESIZE, sizeof(char));
-		
-		strncpy(str, buf, strlen(buf)-1);
-		ans = buf[strlen(buf)-1];
-		h=hash(str);
-    
+    if(*buf == '\n') continue;
+    //this memory never gets freed and shouldn't
+    str = calloc(DBLINESIZE, sizeof(char));
+    strncpy(str, buf, strlen(buf)-1);
+    ans = buf[strlen(buf)-1];
+    h=hash(str);
     char l[10];
     sprintf(l, "%d", strlen(str));
     logdaemonevent(l);
-    
-		if(strcmp(db[h].cardnum, str) == 0) {
-			sprintf(logentry, "\nhash collision for hash: %i from card number: %s creating linked list member.\n", h, str);
-			logdaemonevent(logentry);
+  
+    if(strcmp(db[h].cardnum, str) == 0) {
+      sprintf(logentry, "\nhash collision for hash: %i from card number: %s creating linked list member.\n", h, str);
+      logdaemonevent(logentry);
 			
-			//this memory never gets freed and shouldn't
-			struct simple_rfid_access *rfiddb = calloc(1, sizeof(struct simple_rfid_access));
+      //this memory never gets freed and shouldn't
+      struct simple_rfid_access *rfiddb = calloc(1, sizeof(struct simple_rfid_access));
 			
-			rfiddb->cardnum = str;
-			rfiddb->hashval = ans;
-			rfiddb->collisioncnt++;
-			db[h].next = rfiddb;
-		}
-		else {
-			db[h].cardnum = str; //need to allocate memory here!
-			db[h].hashval = ans;
-			db[h].collisioncnt = 0;
-		}
+      rfiddb->cardnum = str;
+      rfiddb->hashval = ans;
+      rfiddb->collisioncnt++;
+      db[h].next = rfiddb;
+    }
+    else {
+      db[h].cardnum = str; //need to allocate memory here!
+      db[h].hashval = ans;
+      db[h].collisioncnt = 0;
+    }
   }
   return fclose(fp);
   #endif
@@ -211,47 +204,51 @@ load(int argc, char **argv) {
 
 int
 opendaemonlog() {
-	logp = open("/var/log/rfid_daemon.log", O_RDWR | O_APPEND);
-	if(logp < 0) { 
-		printf("failed to open log file: /var/log/rfid_daemon.log");
-		exit(EXIT_FAILURE);
-	}
-	return 1;
+  logp = open("/var/log/rfid_daemon.log", O_RDWR | O_APPEND);
+  if(logp < 0) { 
+    printf("failed to open log file: /var/log/rfid_daemon.log");
+    exit(EXIT_FAILURE);
+  }
+  return 1;
 }
 
 int closedaemonlog() {
-	close(logp);
-	return 1;
+  close(logp);
+  return 1;
 }
 
 int
 logdaemonevent(char *event) {
-	char log[256];
-	log[0] = '\0';
-	time_t t = time(NULL);
-	char *logtime = ctime(&t);
-	logtime[strlen(logtime)-1] = ' ';
-	logtime[strlen(logtime)] = '\0';
-	strcat(log, logtime);
-	strcat(log, event);
-	strcat(log, "\n");
-	write(logp, log, strlen(log));
-	return 1;
+  char log[256];
+  log[0] = '\0';
+  time_t t = time(NULL);
+  char *logtime = ctime(&t);
+  logtime[strlen(logtime)-1] = ' ';
+  logtime[strlen(logtime)] = '\0';
+  strcat(log, logtime);
+  strcat(log, event);
+  strcat(log, "\n");
+  write(logp, log, strlen(log));
+  return 1;
 }
 
 int
 dumpdatabase() {
-	logdaemonevent("starting dump of db\n");
-	int i;
-	for(i=0; i<TABLESIZE; i++) {
-		if(strcmp(db[i].cardnum,"") != 0) {
-			char s[100];
-			sprintf(s, "key: %i --- value: ( cardnum - %s, answer - %c, number of collisions - %i )", i, db[i].cardnum, db[i].hashval, db[i].collisioncnt);
-			logdaemonevent(s);
-		}
-	}
-	logdaemonevent("ending dump of db\n\n");
-	return 1;
+  logdaemonevent("starting dump of db\n");
+  int i,n;
+  n=0;
+  for(i=0; i<TABLESIZE; i++) {
+    if(strcmp(db[i].cardnum,"") != 0) {
+      char s[100];
+      sprintf(s, "key: %i --- value: ( cardnum - %s, answer - %c, number of collisions - %i )", i, db[i].cardnum, db[i].hashval, db[i].collisioncnt);
+      logdaemonevent(s);
+      n++;
+    }
+  }
+  char f[256];
+  sprintf(f, "ending dump of db\n\nbrought in: %d records.\n\n", n);
+  logdaemonevent(f);
+  return 1;
 }
 
 void
@@ -279,14 +276,14 @@ main(int argc, char **argv) {
   clear();
   load(argc, argv);
 
-	sprintf(logentry, "Starting rfid serial daemon, version %s", version);
-	logdaemonevent(logentry);
+  sprintf(logentry, "Starting rfid serial daemon, version %s", version);
+  logdaemonevent(logentry);
   
-	dumpdatabase();
+  dumpdatabase();
 	
   pid = fork();
    
-	if(pid < 0) {	logdaemonevent("failed to fork daemon event handler process."); exit(EXIT_FAILURE); }
+  if(pid < 0) {	logdaemonevent("failed to fork daemon event handler process."); exit(EXIT_FAILURE); }
   if(pid > 0) {	logdaemonevent("forked daemon event handler process."); exit(EXIT_SUCCESS); }
   
   umask(0);
@@ -309,9 +306,9 @@ main(int argc, char **argv) {
   sigaction(SIGIO,&saio,NULL);
   
   /* allow the process to receive SIGIO */
-	char buff[10];
-	sprintf(buff, "pid of event handler: %i", getpid());
-	logdaemonevent(buff);
+  char buff[10];
+  sprintf(buff, "pid of event handler: %i", getpid());
+  logdaemonevent(buff);
 	
   fcntl(fd, F_SETOWN, getpid());
   fcntl(fd, F_SETFL, FASYNC);
